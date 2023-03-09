c8d3cc63a802177194ca30ac79f57288e74f8372
arivusurya <arivusurya.3@gmail.com>
2023-03-09 23:38:47 +0530

    custom exception for user validation

--- User.cs
+++ User.cs
@@ -8,21 +8,41 @@ namespace UserValidation{
 
             public Boolean ValidateName(string firstName){   
                String Pattern = "[A-Z]{1}[a-z]{2,}";
-               return Regex.IsMatch(firstName,Pattern);
+               if(Regex.IsMatch(firstName,Pattern))
+               {
+                return true;
+                 }else{
+                    throw new UserException("username is not valid");
+               }
+               
             }
 
             public Boolean Validatemail(string email){
                 string Pattern ="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.-][a-zA-Z0-9]+)*.[a-zA-Z]{2,}$";
-                return Regex.IsMatch(email,Pattern);
+                if(Regex.IsMatch(email,Pattern)){
+                    return true;
+                }else
+                {
+                    throw new UserException("Not valid email");
+                }
             }
 
             public Boolean validNumber(string Number){
                 string Pattern = "^91 [0-9]{10}";
-                return Regex.IsMatch(Number,Pattern);
+                if(Regex.IsMatch(Number,Pattern)){
+                    return true;
+                }else{
+                    throw new UserException("not a valid Number");
+                }
             }
              public Boolean validPassWord(string Password){
                 string Pattern = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%*.&]).{8,}$";
-                return Regex.IsMatch(Password,Pattern);
+                if(Regex.IsMatch(Password,Pattern)){
+                    return true;
+                }else
+                {
+                    throw new UserException("Not a valid password");
+                }
             }
 
       
